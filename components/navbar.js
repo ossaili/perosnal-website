f789ec152f4b2185b636991f6509353b07a8ea47
ossaili <shadi.ossaili@yandex.com>
2022-03-16 11:13:51 +0200

    1 file modified

--- components/navbar.js
+++ components/navbar.js
@@ -1,7 +1,8 @@
 export default function Navbar() {
   return (
-    <div className="bg-white h-14 drop-shadow-sm flex pl-6 flex-row ">
-      <div className="my-auto">SHADI OSSAILI</div>
+    <div className="bg-white h-14 drop-shadow-sm flex flex-row divide-x">
+      <div className="h-full">SHADI OSSAILI</div>
+      <div className="my-auto  h-14">SHADI OSSAILI</div>
     </div>
   );
 }
