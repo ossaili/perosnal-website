5e9d810ebeef6fbcc244cb2490a1b2036daf4ce9
ossaili <shadi.ossaili@yandex.com>
2022-03-16 13:08:23 +0200

    3 files modified

--- components/navbar.js
+++ components/navbar.js
@@ -1,8 +1,23 @@
 export default function Navbar() {
   return (
     <div className="bg-white h-14 drop-shadow-sm flex flex-row divide-x">
-      <div className="h-full">SHADI OSSAILI</div>
-      <div className="my-auto  h-14">SHADI OSSAILI</div>
+      <div className="h-full flex">
+        <div className="my-auto px-12 tracking-[.35em] font-raleway font-extrabold text-xs	text-zinc-600	hover:text-blue-500	cursor-pointer	">
+          SHADI OSSAILI
+        </div>
+      </div>
+
+      <div className="grid grid-cols-5 gap-0">
+        <NavbarItem label="ABOUT" />
+        <NavbarItem label="RESUME" />
+        <NavbarItem label="PROJECTS" />
+        <NavbarItem label="STATS" />
+        <NavbarItem label="CONTACT" />
+      </div>
     </div>
   );
+}
+
+function NavbarItem({ label }) {
+  return <div>{label}</div>;
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        raleway: ['Raleway', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
